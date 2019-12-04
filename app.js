6f4ba41e519ba2ff472bf766d812a180ee31d0f7
Alex <Alexnguyen@coursera.com>
2019-12-04 21:13:57 +0700

    webapp

--- app.js
+++ app.js
@@ -1,13 +1,40 @@
 (function (){
 'use strict';
-angular.module('webapp',[])
-.controller('webappcontroller',webappcontroller);
+var shoppingList1=[
+  "milk","Donuts","Cookies","Chocolate","Peanut Butter","Pepto Bismol",
+  "Pepto Bismol(chocolate flavor)", "Pepto Bismol(Cookie flavor)"
+];
+var shoppingList2=[
+  {
+    name: "milk",
+    quantity:"2"
+  },
+  {
+    name:"Donuts",
+    quantity:"200"
 
-
-
-
-
-
-
-
+  },
+  {
+    name:"Cookies",
+    quantity:"300"
+  },
+  {
+    name:"Chocolate",
+    quantity:"5"
+  }
+];
+angular.module('ShoppingListApp',[])
+.controller('ShoppingListController',ShoppingListController);
+ShoppingListController.$inject=['$scope'];
+function ShoppingListController($scope){
+  $scope.shoppingList1=shoppingList1;
+  $scope.shoppingList2=shoppingList2;
+  $scope.addtolist = function(){
+    var newitem = {
+      name: $scope.itemname,
+      quantity: $scope.itemquantity
+    };
+    $scope.shoppingList2.push(newitem);
+  };
+}
 })();
